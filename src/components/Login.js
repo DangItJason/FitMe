c9691f289fd02b7742c71cd716b77c855ef760ff
BuildTools <unconfigured@null.spigotmc.org>
2020-04-29 17:57:22 -0400

    Added bootstrap theme to log-n

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
-import Card from 'react-bootstrap/Card';
-import InputGroup from 'react-bootstrap/InputGroup';
-import FormControl from 'react-bootstrap/FormControl';
-import Form from 'react-bootstrap/Form';
+import './Login.css';
 
 class Login extends Component {
 
@@ -25,56 +22,72 @@ class Login extends Component {
 	}
 
 	auth() {
-        fetch("http://localhost:9000/login/login", {
-            method: 'post',
-            body: JSON.stringify(this.state),
-            headers: { //Make sure your header content type you specify and body type match.
-                'Content-Type': 'application/json',
-              },
-        })
-		.then(res => this.setState({
-			login: res.json(),
-		}));
-    }
+		fetch("http://localhost:9000/login/login", {
+			method: 'post',
+			body: JSON.stringify(this.state),
+			headers: { //Make sure your header content type you specify and body type match.
+				'Content-Type': 'application/json',
+			},
+		})
+			.then(res => this.setState({
+				login: res.json(),
+			}));
+	}
 
-    handleSubmit = event => {
-        //We can add true sign up functionality later.
-        event.preventDefault();
-			//console.log(this.state)
-			console.log("Before " + this.state.login);
-			this.auth();
-			console.log("After " + this.state.login);
-            // if (this.auth()) {
-			// 	//Proceed to home page.
-			// 	console.log("Log in success");
-			// }
-    }
+	handleSubmit = event => {
+		//We can add true sign up functionality later.
+		event.preventDefault();
+		//console.log(this.state)
+		console.log("Before " + this.state.login);
+		this.auth();
+		console.log("After " + this.state.login);
+		// if (this.auth()) {
+		// 	//Proceed to home page.
+		// 	console.log("Log in success");
+		// }
+	}
 
 	render() {
 		return (
-			<Card>
-				<Card.Title>Log In</Card.Title>
-				<Form onSubmit={this.handleSubmit}>
-					<InputGroup className="mb-3">
-						<FormControl
-							onChange={this.handleChange}
-							name="email"
-							placeholder="Email"
-							value={this.state.email}
-						/>
-					</InputGroup>
-					<InputGroup className="mb-3">
-						<FormControl
-							onChange={this.handleChange}
-							name="password"
-							placeholder="Password"
-							type="password"
-							value={this.state.password}
-						/>
-					</InputGroup>
-					<button type="submit" variant="primary">Log In</button>
-				</Form>
-			</Card>
+			<div>
+				<div className="sidenav">
+					<div className="login-main-text">
+						<h2>Login</h2>
+						<p>Enter your email and password to access.</p>
+					</div>
+				</div>
+				<div className="main">
+					<div className="col-md-6 col-sm-12">
+						<div className="login-form">
+							<form onSubmit={this.handleSubmit}>
+								<div className="form-group">
+									<label>Email</label>
+									<input
+										type="text"
+										className="form-control"
+										placeholder="Email"
+										name="email"
+										value={this.state.email}
+										onChange={this.handleChange}
+									/>
+								</div>
+								<div className="form-group">
+									<label>Password</label>
+									<input
+										type="password"
+										className="form-control"
+										placeholder="Password"
+										value={this.state.password}
+										name="password"
+										onChange={this.handleChange} />
+								</div>
+								<button type="submit" className="btn btn-black">Login</button>
+								<button type="submit" className="btn btn-secondary">Register</button>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
 		)
 	}
 }
